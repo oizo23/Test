81ff6731bbbc086276ba3cc7e4308dd895708340
oizo23 <ultraspoker@gmail.com>
2018-03-29 13:17:14 +0300

    New VC

---
+++ Test/Test/LoginFormVC.swift
@@ -0,0 +1,35 @@
+//
+//  LoginFormVC.swift
+//  Test
+//
+//  Created by Sergei Morozov on 29.03.2018.
+//  Copyright © 2018 Sergei Morozov. All rights reserved.
+//
+
+import UIKit
+
+class LoginFormVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
